4fa3535398643df8b51f8437e64062d47b5dfb2c
worashf <uppert83@gmail.com>
2022-09-08 16:40:47 +0300

    Add app menus

---
+++ app.rb
@@ -0,0 +1,169 @@
+require_relative 'helpers/menu'
+require_relative 'helpers/create_book'
+require_relative 'helpers/create_game'
+require_relative 'helpers/create_movie'
+require_relative 'helpers/create_music_album'
+require_relative 'helpers/display/list_book'
+require_relative 'helpers/display/list_movie'
+require_relative 'helpers/display/list_game'
+require_relative 'helpers/display/list_music_album'
+require_relative 'helpers/display/list_genre'
+require_relative 'helpers/display/list_label'
+require_relative 'helpers/display/list_source'
+require_relative 'helpers/display/list_author'
+class App
+  def initialize
+    @books = []
+    @games = []
+    @movies = []
+    @music_albums = []
+    @authors = []
+    @genres = []
+    @labels = []
+    @sources = []
+  end
+
+  def display_menu
+    menu = Menu.new
+    menu_options = menu.list_menu
+    selected_menu = gets.chomp.to_i
+    if [*1..4].include? selected_menu
+      listing_child_option(selected_menu)
+    elsif [*5..8].include? selected_menu
+      listing_parent_option(selected_menu)
+    elsif [*9..12].include? selected_menu
+      adding_option(selected_menu)
+    elsif selected_menu == 13
+      quit(selected_menu)
+    else
+      puts 'Invald options'
+    end
+  end
+
+  private
+
+  def listing_child_option(option)
+    loop do
+      case option
+      when 1
+        list_books
+        display_menu
+      when 2
+        list_music_albums
+        display_menu
+      when 3
+        list_movies
+        display_menu
+      when 4
+        list_games
+        display_menu
+      end
+    end
+  end
+
+  def listing_parent_option(option)
+    loop do
+      case option
+      when 5
+        list_genres
+        display_menu
+      when 6
+        list_labels
+        display_menu
+      when 7
+        list_authors
+        display_menu
+      when 8
+        list_sources
+        display_menu
+      end
+    end
+  end
+
+  def adding_option(option)
+    loop do
+      case option
+      when 9
+        add_new_book
+        display_menu
+      when 10
+        add_new_music_album
+        display_menu
+      when 11
+        add_new_game
+        display_menu
+      when 12
+        add_new_movie
+        display_menu
+      end
+    end
+  end
+
+  def add_new_book
+    book = CreateBook.add
+    @books << book
+    @labels << book.label
+    @sources << book.source
+    @genres << book.genre
+    @authors << book.author
+  end
+
+  def add_new_game
+    game = CreateGame.add
+    @games << game
+    @labels << game.label
+    @sources << game.source
+    @genres << game.genre
+    @authors << game.author
+  end
+
+  def add_new_movie
+    movie = CreateMovie.add
+    @movies << movie
+    @labels << movie.label
+    @sources << movie.source
+    @genres << movie.genre
+    @authors << movie.author
+  end
+
+  def add_new_music_album
+    music_album = CreateMusicAlbum.add
+    @music_albums << music_album
+    @labels << music_album.label
+    @sources << music_album.source
+    @genres << music_album.genre
+    @authors << music_album.author
+  end
+
+  def list_books
+    ListBook.display(@books)
+  end
+
+  def list_games
+    ListGame.display(@games)
+  end
+
+  def list_movies
+    ListMovie.display(@movies)
+  end
+
+  def list_music_albums
+    ListMusicAlbum.display(@music_albums)
+  end
+
+  def list_authors
+    ListAuthor.display(@authors)
+  end
+
+  def list_genres
+    ListGenre.display(@genres)
+  end
+
+  def list_sources
+    ListSource.display(@sources)
+  end
+
+  def list_labels
+    ListLabel.display(@labels)
+  end
+end
